e1020d8ba0bba75114065ed6fe8e6d27e706c0c5
b220as <cist.b220.a.shimizu@gmail.com>
2022-10-30 02:35:31 +0900

    kadai 1-2

--- prmn2022a/src/lecture01/Main.java
+++ prmn2022a/src/lecture01/Main.java
@@ -1,7 +1,17 @@
 package lecture01;
 
 public class Main {
+
     public static void main(String[] args) {
+
+        //kadai 1-1
         System.out.println("B2201250 Ayaka Shimizu");
+
+        //kadai 1-2
+        int studentNumber = 2201250 ;
+        System.out.println("B"+studentNumber+" Ayaka Shimizu");
+
+        //kadai 1-3
+
     }
 }
