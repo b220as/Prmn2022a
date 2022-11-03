a780849896f7ec0215347428166209e2e16b635d
b220as <cist.b220.a.shimizu@gmail.com>
2022-11-04 01:06:42 +0900

    課題3-1

---
+++ prmn2022a/src/lecture03/Exercise3_1.java
@@ -0,0 +1,10 @@
+package lecture03;
+
+public class Exercise3_1 {
+    public static void main(String[] args) {
+        System.out.print("任意の文字列を入力: ");
+        java.util.Scanner scanner= new java.util.Scanner(System.in);
+        String str = scanner.next();
+        System.out.println("\""+str+"\"と入力されました");
+    }
+}
