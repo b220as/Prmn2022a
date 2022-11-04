705d47263f1646f412118e46019ab1468f766c16
b220as <cist.b220.a.shimizu@gmail.com>
2022-11-04 16:31:15 +0900

    kadai4_1 途中

---
+++ prmn2022a/src/lecture04/Exercise4_1.java
@@ -0,0 +1,7 @@
+package lecture04;
+
+public class Exercise4_1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ prmn2022a/src/lecture04/Fighter.java
@@ -0,0 +1,15 @@
+package lecture04;
+
+public class Fighter {
+
+    int hitPoint;
+    int power;
+    String name;
+
+    //コンストラクタ
+    Fighter(int hitPoint, int power, String name){
+        this.hitPoint = hitPoint;
+        this.power = power;
+        this.name = name;
+    }
+}
